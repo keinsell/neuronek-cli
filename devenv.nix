0b0b70d41c4918a5efed18869db7997630fb6b30
Jakub Olan <keinsell@protonmail.com>
2024-09-04 06:01:33 +0200

    add `devenv` configuration
    Add configuration of `devenv` tooling to declare one and only
    environment for futher development. Any futher change of development
    environment must be reproduced in this tool.

---
+++ devenv.nix
@@ -0,0 +1,55 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.PROJECT = "neuronek-cli";
+
+  dotenv = {
+    enable = true;
+    disableHint = true;
+  };
+
+  devenv = {
+    debug = true;
+    warnOnNewVersion = true;
+  };
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git openssl onefetch direnv nix-direnv nix-direnv-flakes];
+
+  # https://devenv.sh/languages/
+  languages = {
+    rust = {
+      enable = true;
+      channel = "nightly";
+      rustflags = "-Z threads=8";
+      components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.motd.exec = "onefetch";
+  scripts.build.exec = "cargo build";
+  scripts.test.exec = "cargo test";
+
+  enterShell = ''
+    motd
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
